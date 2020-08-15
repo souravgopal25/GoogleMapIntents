bcafdb308823697e297ca32f4b05a916027648c2
Souarv Sharma <souravgopal25@gmail.com>
2020-08-15 20:28:41 +0530

    SOS feature and Maps completed

--- app/src/main/java/com/example/googlemapintents/MainActivity.java
+++ app/src/main/java/com/example/googlemapintents/MainActivity.java
@@ -1,14 +1,138 @@
 package com.example.googlemapintents;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    @BindView(R.id.btn)
+    Button btn;
+    double x, y;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "ENABLE PERMISSION OF NETWORK", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        onLocationChanged(location);
+
+
+
+
+    }
+
+    @OnClick(R.id.btn)
+    public void onViewClicked() {
+
+        smsSent("6371849902","My LOCATIOn "+x+""+y);
+        Toast.makeText(this, "VALUE OF X,y"+x+"   "+y, Toast.LENGTH_SHORT).show();
+        Uri gmmIntentUri = Uri.parse("geo:"+x+","+y+"?q=hospital");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        x=location.getLongitude();
+        y=location.getLatitude();
+
+        Toast.makeText(this, "LOCATION "+x+" "+y, Toast.LENGTH_SHORT).show();
+        smsSent("6371849902","My LOCATIOn "+x+""+y);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+    void smsSent(String phone,String msg){
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+            }
+        },new IntentFilter(SENT));
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phone, null, msg, sentPI, deliveredPI);
     }
 }
